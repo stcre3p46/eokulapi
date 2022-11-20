d6313e65a6d599b141a2e0c10fffe7b1e7282375
insanolanbiri <erenakgun2007@hotmail.com>
2022-11-20 14:26:19 +0300

    added MarkLesson.mark_to_str() method
    to convert the mark types to human-readable format

--- src/eokulapi/Models/MarkLesson.py
+++ src/eokulapi/Models/MarkLesson.py
@@ -6,7 +6,11 @@ from eokulapi.Models import from_int, from_str, str_to_float
 
 @dataclass
 class MarkLesson:
-    """Mark lesson model"""
+    """Mark lesson model
+
+    Note that each MarkLesson object only contains one term data.
+    Using lesson_id as a primary key among MarkLesson objects is not recommended.
+    Use a combination of lesson_id and term instead."""
 
     lesson: str
     """Name of the lesson"""
@@ -15,7 +19,8 @@ class MarkLesson:
     lesson_weekly_period: int
     """Weekly period count of the lesson"""
     term: int
-    """Term of the lesson"""
+    """Term of the lesson
+    it is even for the first term and odd for the second term"""
     isExempt: bool
     """Whether the student is exempt from the lesson"""
     odv: Any
@@ -25,9 +30,31 @@ class MarkLesson:
     score: float
     """Score of the student in the lesson"""
     sozlu: dict[int, float]
-    """Dict of `sözlü` exam scores (Exam Number: Score)"""
+    """Dict of `sözlü` exam scores (Exam Number: Score)
+    
+    Dict keys are from 1 to 6, but not all of them are present as `sozlu`:
+    1: `performans1`
+    2: `performans2`
+    3: `performans3`
+    4: `uygulama1`
+    5: `uygulama2`
+    6: `uygulama3`
+    mark_to_str() method can be used to convert the keys to string representations of the marks.
+    
+    Note that all of the keys are *not* present in all of the lessons."""
     yazili: dict[int, float]
-    """Dict of `yazılı` exam scores (Exam Number: Score)"""
+    """Dict of `yazılı` exam scores (Exam Number: Score)
+    
+    Dict keys are from 1 to 6, but not all of them are present as `yazili`:
+    1: `sınav1`
+    2: `sınav2`
+    3: `sınav3`
+    4: `sınav4`
+    5: `sınav5`
+    6: `ortak_sınav`
+    mark_to_str() method can be used to convert the keys to string representations of the marks.
+    
+    Note that all of the keys are *not* present in all of the lessons."""
 
     @classmethod
     def from_dict(cls, obj: dict):
@@ -61,3 +88,38 @@ class MarkLesson:
                 yazili[i] = str_to_float(dat)
 
         return cls(ders, ders_kodu, ders_saati, donem, muaf, odv, tpu, puan, sozlu, yazili)
+
+    def mark_to_str(self, mark_type: bool, mark_no: int) -> str:
+        """Converts a mark number to a string representation of the mark
+
+        Args:
+            mark_type (bool): Whether the mark is `yazili` or `sozlu`.
+            mark_no (int): Number of the mark, from 1 to 6
+
+        Raises:
+            AssertionError: If arguments are invalid
+
+        Returns:
+            str: String representation of the mark
+        """
+
+        assert mark_type in [True, False]
+        assert mark_no in range(1, 7)
+
+        string = ""
+        if self.term % 2 == 0:
+            string += "1. dönem"
+        else:
+            string += "2. dönem"
+        if mark_type:  # yazili
+            if 1 <= mark_no <= 5:
+                string += f"{mark_no}. yazılı"
+            elif mark_no == 6:
+                string += "Ortak yazılı"
+        else:  # sozlu
+            if 1 <= mark_no <= 3:
+                string += f"{mark_no}. performans"
+            elif 4 <= mark_no <= 6:
+                string += f"{mark_no-3}. uygulama"
+
+        return string
