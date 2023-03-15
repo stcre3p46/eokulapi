2375c1f370791b785e6787e1ddb1e2487a83f264
insanolanbiri <erenakgun2007@hotmail.com>
2023-03-15 20:22:45 +0300

    add token refresh unit test

--- tests/test_eokulapi.py
+++ tests/test_eokulapi.py
@@ -26,6 +26,13 @@ def test_get_student():
     student = api.students[0]
     assert "EREN A" in student.name, "Student name is not what expected"
 
+def test_update_student_token():
+    api = EokulAPI(uid)
+    student = api.students[0]
+    prev = student.endterm_marks
+    api.student_dict[student.tckn] = (student, "dummy")
+    api._update_endterm_marks(student)
+    assert prev != student.endterm_marks, "Student token is not updated properly"
 
 def test_get_student_grades():
     api = EokulAPI(uid)
